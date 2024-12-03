e6e3321b5a93c7b73bd09b81d29e77b10cacb45c
glyndaja <glyndaja@student.21-school.ru>
2024-12-03 13:24:10 +0300

    Added option to choose file

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,37 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        LogFileReader fileReader = new LogFileReader("C:\\Users\\User\\IdeaProjects\\untitled\\test\\log.txt");
-        String[] logLines = fileReader.readFile();
+        JFileChooser fileChooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Files (*.txt)", "txt");
+        fileChooser.setFileFilter(filter);
 
-        if (logLines.length == 0) {
-            System.out.println("Failed to read log file.");
-            return;
+        int result = fileChooser.showOpenDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            String filePath = selectedFile.getAbsolutePath();
+
+            LogFileReader fileReader = new LogFileReader(filePath);
+            String[] logLines = fileReader.readFile();
+            if (logLines.length == 0) {
+                System.out.println("Failed to read log file.");
+                return;
+            }
+            LogsProcessor processor = new LogsProcessor(Arrays.asList(logLines));
+
+            Map<String, List<LogEntry>> groupedByMethod = processor.groupByMethod();
+            System.out.println("Group by Method: \n" + groupedByMethod + "\n\n");
+
+            Map<String, List<LogEntry>> groupedByEndpoint = processor.groupByEndpoint();
+            System.out.println("Group by Endpoint: \n" + groupedByEndpoint);
+        } else {
+            System.out.println("User cancelled file selection.");
         }
-
-        LogsProcessor processor = new LogsProcessor(Arrays.asList(logLines));
-
-        Map<String, List<LogEntry>> groupedByMethod = processor.groupByMethod();
-        System.out.println("Group by Method: \n" + groupedByMethod + "\n\n");
-
-        Map<String, List<LogEntry>> groupedByEndpoint = processor.groupByEndpoint();
-        System.out.println("Group by Endpoint: \n" + groupedByEndpoint);
     }
 }
